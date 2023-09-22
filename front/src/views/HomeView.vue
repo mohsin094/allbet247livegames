d0a35a5cbd7538ec352a9f11b6df017bf8da426c
titi <tkamalipoor@gmail.com>
2023-09-22 22:47:33 +0330

    bgammon header and layout update

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,16 +1,25 @@
 <template>
-  <main v-if="!isGuest">
-  	<MyGames v-if="!$user.data.isGuest"/>
-  	<Tabs/>
-  </main>
+  <div class="row">
+  	<Sidebar/>
+  	<div class="col-md-10 col-xl-10 px-sm-2 px-0">
+	  <div class="main-wrapper min-vh-100">
+		  <main v-if="!isGuest">
+		  	<MyGames v-if="!$user.data.isGuest"/>
+		  	<Tabs/>
+		  </main>
+	  </div>
+	</div>
+</div>
 </template>
 <script>
 	import MyGames from '@/components/MyGames.vue';
 	import Tabs from '@/components/Tabs.vue';
+	import Sidebar from '@/components/Sidebar.vue';
 	export default {
 		components: {
 		    MyGames,
-		    Tabs
+		    Tabs,
+		    Sidebar
 	  	},
 	};
 </script>
