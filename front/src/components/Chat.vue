2e641d44e6a1de9012f50c930bcb2a2accf05b55
titi <tkamalipoor@gmail.com>
2023-10-03 12:33:05 +0330

    update

--- front/src/components/Chat.vue
+++ front/src/components/Chat.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="chat-body" class="px-2">
 		<ul>
-			<li v-for="chat in chats" class="message-box">{{chat}}</li>
+			<li v-for="chat in chats" class="message-box"><p class="mb-0">{{chat}}</p></li>
 		</ul>
 	</div>
 </template>
