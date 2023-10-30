585b8155d1ae5caeba08c0fbadb039332edc87e8
Omid Abedi <oabedi2011@gmail.com>
2023-10-30 18:57:23 +0100

    init admin template

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,11 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import user from "./plugins/User";
 const app = createApp(App)
 
-app.use(router)
+app
+.use(user)
+.use(router)
+
+app.config.globalProperties.$storage = window.localStorage;
 
 app.mount('#app')
