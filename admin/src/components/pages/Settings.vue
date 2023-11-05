2bfc02aa6c4e27881d629b66d30eca8b663109ad
Omid Abedi <oabedi2011@gmail.com>
2023-11-05 14:41:03 +0100

    settings page finished

--- admin/src/components/pages/Settings.vue
+++ admin/src/components/pages/Settings.vue
@@ -1,3 +1,55 @@
 <template>
 <h4>Settings</h4>
+<div class="card">
+	<div class="table-responsive text-nowrap">
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Value</th>
+					<th>DESC.</th>
+				</tr>
+			</thead>
+			<tbody class="table-border-bottom-0">
+				<tr v-for="setting in Object.keys(settings)">
+					<td>
+						{{settings[setting].name}}
+					</td>
+					<td>
+						{{settings[setting].value}}
+					</td>
+					<td>
+						{{settings[setting].description}}
+					</td>
+
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
 </template>
+<script>
+	export default {
+		data()
+		{
+			return {
+				settings: {}
+			}
+		},
+		mounted()
+		{
+			this.fetchSettings();
+		},
+		methods: {
+			fetchSettings()
+			{
+				this.$axios.get(import.meta.env.VITE_BACKEND_BASE_URL + "/admin-setting/get").then((res) => {
+					res = res.data;
+					if(res.result) {
+						this.settings = res.params;
+					}
+				});
+			}
+		}
+	}
+</script>
