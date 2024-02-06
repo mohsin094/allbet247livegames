1070f8d62f1e742c2051acc1ca2e0d54b7b8a4e6
titi <tkamalipoor@gmail.com>
2024-02-06 16:36:20 +0330

    fix poker

--- backend/modules/poker/controllers/DefaultController.php
+++ backend/modules/poker/controllers/DefaultController.php
@@ -18,13 +18,23 @@ class DefaultController extends ApiController
                 'class' => AccessControl::class,
                 'rules' => [
                     [
-                        'actions' => ['get-balance'],
+                        'actions' => ['get-balance', 'get-session'],
                         'roles' => ['@'],
                         'allow' => true,
                     ],
                 ],
             ]
 		]);
+	}
+
+	public function actionGetSession()
+	{
+		$url = \Yii::$app->mavens->account->mavensUrl(\Yii::$app->user->getIdentity()->public_name);
+
+		$this->resp->result = true;
+		$this->resp->params = ['url' => $url];
+
+		return $this->resp;
 	}
 
 	public function actionGetBalance()
--- front/src/views/Poker.vue
+++ front/src/views/Poker.vue
@@ -1,19 +1,35 @@
 <template>
 	<div class="row">
 		<div class="ratio ratio-16x9">
-		  <iframe :src="$user.data.mavensUrl" allowfullscreen></iframe>
+		  <iframe :src="url" allowfullscreen></iframe>
 		</div>
 	</div>
 </template>
 <script>
 	import { Modal } from 'bootstrap';
 	export default{
+		data() {
+			return {
+				url: ''
+			}
+		},
 		mounted(){
 	   	if(this.$user.data.isGuest){
 	   		var modalEl = document.getElementById('login');
             var modal = new Modal(modalEl)
             modal.show();
 	   	}
+
+	   	this.fetchUrl();
 	   },
+	   methods: {
+	   	fetchUrl() {
+	   		const url = import.meta.env.VITE_BACKEND_BASE_URL+'/poker/default/get-session';
+			this.$axios.get(url).then(res => {
+				res = res.data;
+				this.url = res.params.url;
+			});
+	   	}
+	   }
 	}
 </script>
